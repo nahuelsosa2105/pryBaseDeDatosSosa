40bea5d35993029edc19190e66fa59899055ef0c
Alumnos <Alumnos@PC52>
2023-09-26 14:26:44 -0300

    Agregar archivos de proyecto.

---
+++ frmBaseDeDatos.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace pryBaseDeDatosSosa
+{
+    partial class frmBaseDeDatos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectarBase = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConectarBase
+            // 
+            this.btnConectarBase.Location = new System.Drawing.Point(23, 38);
+            this.btnConectarBase.Name = "btnConectarBase";
+            this.btnConectarBase.Size = new System.Drawing.Size(75, 23);
+            this.btnConectarBase.TabIndex = 0;
+            this.btnConectarBase.Text = "Conectar";
+            this.btnConectarBase.UseVisualStyleBackColor = true;
+            this.btnConectarBase.Click += new System.EventHandler(this.btnConectarBase_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(20, 79);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // frmBaseDeDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 442);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnConectarBase);
+            this.Name = "frmBaseDeDatos";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectarBase;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ frmBaseDeDatos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using System.Data.OleDb;
+
+namespace pryBaseDeDatosSosa
+{
+    public partial class frmBaseDeDatos : Form
+    {
+        OleDbConnection conexionBD;
+        public frmBaseDeDatos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectarBase_Click(object sender, EventArgs e)
+        {
+            conexionBD = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source =E:\\Escritorio\\pryBaseDeDatosSosa\\BASE DE DATOS\\2_EL_CLUB.accdb");
+            conexionBD.Open();
+
+            lblResultado.Text = "Conectado a la base";
+            lblResultado.BackColor = Color.Green;
+        }   
+    }
+}
